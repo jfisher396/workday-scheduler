c4840fc1ec99f4f5bc8c4ce2b24a3be4ef73da1b
James Fisher <jfisher396@hotmail.com>
2020-06-20 21:28:45 -0700

    time blocks are compared against actual time

--- script.js
+++ script.js
@@ -4,22 +4,50 @@ let displayDate = document.getElementById('currentDay');
 displayDate.innerHTML = NowMoment;
 let currentHour = moment().format("HH");
 
-console.log(currentHour)
+console.log(currentHour);
 
-let nine = $("#9");
-let ten = $("#10");
-let eleven = $("#11");
-let twelve = $("#12");
-let thirteen = $("#13");
-let fourteen = $("#14");
-let fifteen = $("#15");
-let sixteen = $("#16");
-let seventeen = $("#17");
+let nine = $("#9").text();
+let ten = $("#10").text();
+let eleven = $("#11").text();
+let twelve = $("#12").text();
+let thirteen = $("#13").text();
+let fourteen = $("#14").text();
+let fifteen = $("#15").text();
+let sixteen = $("#16").text();
+let seventeen = $("#17").text();
+console.log(fourteen);
 
-let plannerHour = nine;
+let intFromNine = parseInt(nine);
+let intFromTen = parseInt(ten);
+let intFromEleven = parseInt(eleven);
+let intFromTewelve = parseInt(twelve);
+let intFromThirteen = parseInt(thirteen) + 12;
+let intFromFourteen = parseInt(fourteen) + 12;
+let intFromFifteen = parseInt(fifteen) + 12;
+let intFromSixteen = parseInt(sixteen) + 12;
+let intFromSeventeen = parseInt(seventeen) + 12;
+console.log(intFromSeventeen)
+
+console.log("Here's the int that was in #9 : ", intFromNine);
 
 
-
-function timeComparison (currentHour, plannerHour) {
-    
+$(".time-div").each(function(){
+    var timeDiv = $(this).attr("id").split("-")[1];
+    console.log("timeDiv:", timeDiv);
+    if (currentHour === timeDiv) {
+        console.log("equal");
+        $(this).addClass("present");
+    } else if (currentHour < timeDiv) {
+        console.log("less");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+        console.log("greater");
+        $(this).removeClass("future");
+        $(this).addClass("past");
     }
+
+})
+
+//"hour-9" -> split(-) => ["hour","9"]
+    
